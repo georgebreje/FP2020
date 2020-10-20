03e5589c9f275e26067be19b6bee0dbbc90a7e64
George Breje <george.breje2@gmail.com>
2020-10-20 09:23:06 +0300

    suma gauss

---
+++ 3_1020/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar natural ");
+            int n = int.Parse(Console.ReadLine());
+            int suma;
+            suma = n * (n + 1) / 2;
+            Console.WriteLine($"Suma numerelor de la 1 la {n} este {suma}");
+        }
+    }
+}
